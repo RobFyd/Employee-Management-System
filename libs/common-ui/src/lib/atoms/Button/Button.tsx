32d552e216c784b1c7d15ff59a94d18c5d64475a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-04 00:26:33 +0100

    refactor: Update Button component to use color variables for background and text colors

--- libs/common-ui/src/lib/atoms/Button/Button.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.tsx
@@ -1,3 +1,12 @@
+
+// better for security:
+// const colors: Record<"emerald" | "peterRiver" | "amethyst", string> = {
+//   emerald: "#2ecc71",
+//   peterRiver: "#3498db",
+//   amethyst: "#9b59b6",
+//   red: "#ff0000",
+// };
+
 const colors = {
   emerald: "#2ecc71",
   peterRiver: "#3498db",
@@ -13,8 +22,11 @@ type Props = {
 };
 
 export const Button = ({ label, bgColor, color }: Props) => {
+  const _color = color ? colors[color] : "";
+  const _bgColor = bgColor ? colors[bgColor] : "";
+
   return <button style={{
-    backgroundColor: bgColor,
-    color,
+    backgroundColor: _bgColor,
+    color: _color,
   }}>{label}</button>;
 };
