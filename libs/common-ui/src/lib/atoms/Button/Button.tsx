92cf7b5595eeb54a6fe042b9cbfd53b2bcca6b8b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-03 23:53:45 +0100

    feat: Add background color and text color options to Button component

--- libs/common-ui/src/lib/atoms/Button/Button.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.tsx
@@ -1,7 +1,20 @@
+const colors = {
+  emerald: "#2ecc71",
+  peterRiver: "#3498db",
+  amethyst: "#9b59b6",
+}
+
+type ColorType = keyof typeof colors;
+
 type Props = {
   label: string;
+  bgColor?: ColorType;
+  color?: ColorType;
 };
 
-export const Button = ({ label }: Props) => {
-  return <button>{label}</button>;
+export const Button = ({ label, bgColor, color }: Props) => {
+  return <button style={{
+    backgroundColor: bgColor,
+    color,
+  }}>{label}</button>;
 };
