c7c162d967ed4f7085bde05bac4574f940cc97fb
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-06 01:16:01 +0100

    rest of colors added

--- libs/common-ui/src/lib/atoms/Button/Button.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.tsx
@@ -14,9 +14,26 @@ import clsx from "clsx";
 import { MouseEventHandler } from "react";
 
 const colors = {
+  turquise: "#1abc9c",
   emerald: "#2ecc71",
   peterRiver: "#3498db",
   amethyst: "#9b59b6",
+  wetAsphalt: "#34495e",
+  greenSea: "#16a085",
+  nephritis: "#27ae60",
+  belizeHole: "#2980b9",
+  wisteria: "#8e44ad",
+  midnightBlue: "#2c3e50",
+  sunFlower: "#f1c40f",
+  carrot: "#e67e22",
+  alizarin: "#e74c3c",
+  clouds: "#ecf0f1",
+  concrete: "#95a5a6",
+  orange: "#f39c12",
+  pumpkin: "#d35400",
+  pomegranate: "#c0392b",
+  silver: "#bdc3c7",
+  asbestos: "#7f8c8d",
 }
 
 type ColorType = keyof typeof colors;
@@ -56,31 +73,31 @@ export const Button = ({
   const _color = color ? colors[color] : "";
   const _bgColor = bgColor ? colors[bgColor] : "";
 
-const classes = clsx(
-  "px-4 py-1", // padding
-  "text-sm text-white", // text size and color
-  "bg-blue-600", // background color
-  "font-semibold", // font weight
-  "rounded-full border border-blue-200", // border
-  "hover:text-white hover:bg-blue-500 hover:border-transparent", // hover effects
-  className, // custom classes
-  // "focus:outline-none", // focus effects
-  // "focus:ring-2",
-  // "focus:ring-offset-2",
-  // "focus:ring-offset-gray-800",
-  // "focus:ring-white",
-  // "transition-colors", // transition effects
-  // "duration-300", // duration
-  // "ease-in-out",
-  // "hover:bg-opacity-80",
-  // "active:bg-opacity-100", // active effects
-  // "disabled:opacity-50", // disabled effects
-  // "disabled:cursor-not-allowed",
-  // "disabled:hover:bg-opacity-100",
-  // "disabled:active:bg-opacity-100",
-  // "disabled:focus:ring-offset-gray-800",
-  // "disabled:focus:ring-white",
-);
+  const classes = clsx(
+    "px-4 py-1", // padding
+    "text-sm text-white", // text size and color
+    "bg-blue-600", // background color
+    "font-semibold", // font weight
+    "rounded-full border border-blue-200", // border
+    "hover:text-white hover:bg-blue-500 hover:border-transparent", // hover effects
+    className, // custom classes
+    // "focus:outline-none", // focus effects
+    // "focus:ring-2",
+    // "focus:ring-offset-2",
+    // "focus:ring-offset-gray-800",
+    // "focus:ring-white",
+    // "transition-colors", // transition effects
+    // "duration-300", // duration
+    // "ease-in-out",
+    // "hover:bg-opacity-80",
+    // "active:bg-opacity-100", // active effects
+    // "disabled:opacity-50", // disabled effects
+    // "disabled:cursor-not-allowed",
+    // "disabled:hover:bg-opacity-100",
+    // "disabled:active:bg-opacity-100",
+    // "disabled:focus:ring-offset-gray-800",
+    // "disabled:focus:ring-white",
+  );
 
   return <button
     onClick={onClick}
