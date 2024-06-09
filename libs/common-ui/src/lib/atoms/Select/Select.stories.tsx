842368880e4801d96def10c38673f71cc175eeb4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-09 23:48:58 +0100

    stories for select added

---
+++ libs/common-ui/src/lib/atoms/Select/Select.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Select } from './Select';
+
+const meta = {
+    title: 'UI/Atoms/Input',
+    component: Select,
+    tags: ['autodocs'],
+} satisfies Meta<typeof Select>;
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+    args: {
+        name: 'select',
+        id: 'select',
+        size: 3,
+        required: true,
+    },
+};
+
+export const Primary: Story = {
+    args: {},
+};
+
+export const Secondary: Story = {
+    args: {},
+};
