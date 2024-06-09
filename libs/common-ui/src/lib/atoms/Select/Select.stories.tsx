fcee4d0ad36eaed0774bab8f424dce81508310bd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-10 00:00:40 +0100

    bug fixed

--- libs/common-ui/src/lib/atoms/Select/Select.stories.tsx
+++ libs/common-ui/src/lib/atoms/Select/Select.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Select } from './Select';
 
 const meta = {
-    title: 'UI/Atoms/Input',
+    title: 'UI/Atoms/Select',
     component: Select,
     tags: ['autodocs'],
 } satisfies Meta<typeof Select>;
