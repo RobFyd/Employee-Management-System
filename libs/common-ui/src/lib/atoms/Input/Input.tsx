ced347c6f6f2049e557da6d51b0474cca642bdf4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-18 00:08:25 +0100

    Refactor Input component and add Label component

--- libs/common-ui/src/lib/atoms/Input/Input.tsx
+++ libs/common-ui/src/lib/atoms/Input/Input.tsx
@@ -1,9 +1,9 @@
-type Props = {
-    type: string;
-    placeholder: string;
-    name: string;
-};
+// type Props = {
+//     type: string;
+//     placeholder: string;
+//     name: string;
+// };
 
-export const Input = ({ type, placeholder, name }: Props) => {
-    return <input type={type} placeholder={placeholder} name={name} />;
-};
+// export const Input = ({ type, placeholder, name }: Props) => {
+//     return <input type={type} placeholder={placeholder} name={name} />;
+// };
