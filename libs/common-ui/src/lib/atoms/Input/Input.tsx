4a73c71b35e912055292bfe84c07057ce8a16f13
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-18 00:14:01 +0100

    Input from atoms removed

--- libs/common-ui/src/lib/atoms/Input/Input.tsx
+++
@@ -1,9 +0,0 @@
-// type Props = {
-//     type: string;
-//     placeholder: string;
-//     name: string;
-// };
-
-// export const Input = ({ type, placeholder, name }: Props) => {
-//     return <input type={type} placeholder={placeholder} name={name} />;
-// };
