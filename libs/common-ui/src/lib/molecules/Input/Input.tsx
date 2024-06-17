2dbd14e1ccc2b73b372f83850626b65dc65f6456
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-18 00:56:43 +0100

    chore: Update Label and Input components with styling improvements

--- libs/common-ui/src/lib/molecules/Input/Input.tsx
+++ libs/common-ui/src/lib/molecules/Input/Input.tsx
@@ -10,7 +10,13 @@ export const Input = ({ label, type, value, onChange, ...rest }: Props) => {
     return (
         <div className="mb-2">
             <Label id={id}>{label}</Label>
-            <input id={id} type={type} defaultValue={value} onChange={onChange} {...rest} />
+            <input
+                id={id}
+                type={type}
+                defaultValue={value}
+                onChange={onChange}
+                className="block w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-800 dark:text-white dark:border-gray-700 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+                {...rest} />
         </div>
     );
 };
