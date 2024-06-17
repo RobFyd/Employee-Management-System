280833bc41eacc2082842ca496fba027dd141178
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-18 00:35:51 +0100

    chore: Update title in Label.stories.tsx to fix incorrect component name

--- libs/common-ui/src/lib/atoms/Label/Label.stories.tsx
+++ libs/common-ui/src/lib/atoms/Label/Label.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Label } from './Label';
 
 const meta = {
-  title: 'UI/atoms/Button',
+  title: 'UI/atoms/Label',
   component: Label,
   tags: ['autodocs'],
 } satisfies Meta<typeof Label>;
