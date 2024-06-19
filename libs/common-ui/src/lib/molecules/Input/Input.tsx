482c6db572c46d3a3d67ce57b0d1f5e216fdc3dd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-19 01:58:04 +0100

    Refactor Input component styling for disabled and invalid states

--- libs/common-ui/src/lib/molecules/Input/Input.tsx
+++ libs/common-ui/src/lib/molecules/Input/Input.tsx
@@ -15,7 +15,12 @@ export const Input = ({ label, type, value, onChange, ...rest }: Props) => {
                 type={type}
                 defaultValue={value}
                 onChange={onChange}
-                className="block w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-800 dark:text-slate-300 dark:border-gray-700 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed
+                invalid:border-pink-500 invalid:text-pink-600
+                focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+                dark:bg-gray-800 dark:text-slate-300 dark:border-gray-700 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
                 {...rest}
             />
         </div>
