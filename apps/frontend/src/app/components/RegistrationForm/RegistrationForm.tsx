124ee7862d711dd1b250a75988e1a710d836e8b9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-20 00:25:48 +0100

    back to useState for Input age

--- apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
+++ apps/frontend/src/app/components/RegistrationForm/RegistrationForm.tsx
@@ -6,10 +6,10 @@ import "./RegistrationForm.module.css";
 export const RegistrationForm = () => {
     // const [firstName, setFirstName] = useState('');
     // const [lastName, setLastName] = useState('');
-    // const [age, setAge] = useState('');
+    const [age, setAge] = useState(0);
     const firstNameRef = useRef<HTMLInputElement>(null);
     const lastNameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
+    // const ageRef = useRef<HTMLInputElement>(null);
 
     const handleSubmit: FormEventHandler = (event) => {
         event.preventDefault();
@@ -18,17 +18,24 @@ export const RegistrationForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <Input label="First name:" ref={firstNameRef} />
-            </div>
-            <div>
-                <Input label="Last name: " ref={lastNameRef} />
-            </div>
-            <div>
-                <Input label="Age: " id="age" type="number" ref={ageRef} />
-            </div>
-            <Button type="submit" label="Send" />
-        </form>
+        <div>
+            <div>Current age: {age}</div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <Input label="First name:" ref={firstNameRef} />
+                </div>
+                <div>
+                    <Input label="Last name: " ref={lastNameRef} />
+                </div>
+                <div>
+                    <Input
+                        label="Age: "
+                        type="number"
+                        onChange={(e) => setAge(parseInt(e.target.value, 10))}
+                    />
+                </div>
+                <Button type="submit" label="Send" />
+            </form>
+        </div>
     )
 };
