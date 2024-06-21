9d443e6bdf2be7b9d7a33a3860ad41e6d1f8eea1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-21 23:30:10 +0100

    chore: Refactor useViewPort hook to track viewport dimensions and handle resize events

--- libs/common-ui/src/lib/hooks/useViewPort.ts
+++ libs/common-ui/src/lib/hooks/useViewPort.ts
@@ -1,11 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 export const useViewPort = () => {
-    const [viewPort, setViewPort] = useState({
+  const [viewPort, setViewPort] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewPort({
         width: window.innerWidth,
         height: window.innerHeight,
-    });
+      });
+    };
 
-    return viewPort;
+    window.addEventListener('resize', handleResize);
+  });
+
+  return viewPort;
 };
-    
