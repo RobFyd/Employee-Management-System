7aa1918ceb37cb2cb5dec145a0a520a1aac63b54
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-25 00:41:43 +0100

    feat: Add routing for HomePage, AboutPage, ContactPage, and EmployeesPage

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
+import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
+import { ContactPage } from "./pages/ContactPage";
+import { EmployeesPage } from "./pages/EmployeesPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'employees',
+        element: <EmployeesPage />,
+      },
+    ],
   },
 ]);
 
