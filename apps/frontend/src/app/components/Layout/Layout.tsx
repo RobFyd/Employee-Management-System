67a688489b150a145809e12f127582518e74020d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-25 00:50:17 +0100

    feat: Update home link in Layout component

--- apps/frontend/src/app/components/Layout/Layout.tsx
+++ apps/frontend/src/app/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="home">Home</Link>
                     </li>
                     <li>
                         <Link to="/about">About</Link>
