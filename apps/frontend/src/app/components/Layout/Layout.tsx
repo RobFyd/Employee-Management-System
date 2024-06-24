5c2d561b26eae4c29e4a2d41f6f8349e8c843c81
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-25 00:48:20 +0100

    feat: Add navigation links to Layout component

--- apps/frontend/src/app/components/Layout/Layout.tsx
+++ apps/frontend/src/app/components/Layout/Layout.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 
 export const Layout = () => {
     return (
         <div>
-            <nav></nav>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/employees">Employees</Link>
+                    </li>
+                </ul>
+            </nav>
             <Outlet />
             <footer>&copy; 2024 MEWA</footer>
         </div>
