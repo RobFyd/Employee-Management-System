b686fdb6f16100b4193a4830038bdb5747d701c6
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-25 01:29:48 +0100

    refactor: Update Layout component to use Menu and Footer components

--- apps/frontend/src/app/components/Layout/Layout.tsx
+++ apps/frontend/src/app/components/Layout/Layout.tsx
@@ -1,28 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
-import { ROUTE } from "../../routes";
-
+import { Menu } from "./Menu";
+import { Outlet } from "react-router-dom";
+import { Footer } from "./Footer";
 
 export const Layout = () => {
     return (
         <div>
-            <nav>
-                <ul className="flex list-none">
-                    <li className="mx-2">
-                        <Link to={ROUTE.HOME}>Home</Link>
-                    </li>
-                    <li className="mx-2">
-                        <Link to={ROUTE.ABOUT}>About</Link>
-                    </li>
-                    <li className="mx-2">
-                        <Link to={ROUTE.CONTACT}>Contact</Link>
-                    </li>
-                    <li className="mx-2">
-                        <Link to={ROUTE.EMPLOYEES}>Employees</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Menu />
             <Outlet />
-            <footer>&copy; 2024 MEWA</footer>
+            <Footer />
         </div>
     )
 };
