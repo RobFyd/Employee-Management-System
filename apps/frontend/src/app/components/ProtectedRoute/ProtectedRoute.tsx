45bc0694b3d3e18211b5909a5b13519fc32d0f80
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-27 00:24:49 +0100

    feat: Add lazy loading for images in ProtectedRoute component

--- apps/frontend/src/app/components/ProtectedRoute/ProtectedRoute.tsx
+++ apps/frontend/src/app/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,3 +1,5 @@
+import { Navigate } from "react-router-dom";
+
 type Props = {
     user: boolean;
     children: React.ReactNode;
@@ -7,4 +9,5 @@ export const ProtectedRoute = ({ children, user = false }: Props) => {
     if (user) {
         return children;
     }
+    return <Navigate to="/registration" />;
 };
