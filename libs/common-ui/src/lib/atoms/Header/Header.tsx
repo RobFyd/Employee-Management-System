fb1d9625af36e030e483c32d54542175702431fe
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-28 00:14:38 +0100

    refactor: Update Header component to use ComponentProps type

--- libs/common-ui/src/lib/atoms/Header/Header.tsx
+++ libs/common-ui/src/lib/atoms/Header/Header.tsx
@@ -1,7 +1,10 @@
+import type { ComponentProps } from "react";
+
 type Props = Readonly<{
-    children: string;
-  }>;
-  
-  export const Header = ({ children }: Props) => {
-    return <h1 className="text-3xl font-bold my-4">{children}</h1>;
-  };
+  children: string | undefined;
+}>;
+
+export const Header = ({ children }: ComponentProps<'header'> & Props) => {
+  return <header className="text-3xl font-bold my-3">{children}</header>;
+  // return <header className="text-3xl font-bold my-3 dark:text-white">{children}</header>;
+};
