99f5064abbe1332f9da48258e5e9723a2b69cff9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-30 00:58:58 +0100

    data added to EmployeesList

--- apps/frontend/src/app/features/employees/EmployeesList.tsx
+++ apps/frontend/src/app/features/employees/EmployeesList.tsx
@@ -1,9 +1,31 @@
 import { Header } from "@ems/common-ui"
 
+const data = [{
+    name: {
+        first: 'John',
+        last: 'Kowalski'
+    },
+    email: 'john@kowalski.en',
+    id: {
+        name: '123'
+    }
+},
+{
+    name: {
+        first: 'Eleonora',
+        last: 'Smith'
+    },
+    email: 'eleonora@smith.en',
+    id: {
+        name: '456'
+    }
+}]
+
 export const EmployeesList = () => {
     return (
         <div>
             <Header>List</Header>
+            {data.map()}
         </div>
     )
 }
