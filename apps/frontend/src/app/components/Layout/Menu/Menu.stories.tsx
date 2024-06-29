a48affc80fa1c78f4319eabe428a3188c9d0eac5
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-29 23:39:55 +0100

    feat: Add pages to Menu component in storybook

--- apps/frontend/src/app/components/Layout/Menu/Menu.stories.tsx
+++ apps/frontend/src/app/components/Layout/Menu/Menu.stories.tsx
@@ -2,6 +2,11 @@ import { Meta, StoryObj } from '@storybook/react';
 import { Menu } from '../Menu';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { ROUTE } from '../../../routes';
+import { AboutPage } from '../../../pages/AboutPage';
+import { ContactPage } from '../../../pages/ContactPage';
+import { EmployeesPage } from '../../../pages/EmployeesPage';
+import { HomePage } from '../../../pages/HomePage';
+import { RegistrationPage } from '../../../pages/RegistrationPage';
 
 const meta = {
     title: 'Components/Menu',
@@ -11,9 +16,14 @@ const meta = {
 export default meta;
 
 export const _Menu = () => (
-    <MemoryRouter initialEntries={[ROUTE.ABOUT]}>
+    <MemoryRouter initialEntries={['/']}>
         <Routes>
-            <Route path={ROUTE.ABOUT} element={<Menu />} />
+            <Route path={"/"} element={<Menu />} />
+            <Route path={ROUTE.ABOUT} element={<AboutPage />} />
+            <Route path={ROUTE.CONTACT} element={<ContactPage />} />
+            <Route path={ROUTE.EMPLOYEES} element={<EmployeesPage />} />
+            <Route path={ROUTE.HOME} element={<HomePage />} />
+            <Route path={ROUTE.REGISTRATION} element={<RegistrationPage />} />
         </Routes>
     </MemoryRouter>
 );
