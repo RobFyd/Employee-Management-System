a396b364653e5e3c193c71b8e6ff0256286f2a04
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-05 01:13:00 +0100

    chore: Update jestConfig in project.json for common-ui library

--- libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
@@ -1,5 +1,5 @@
-describe('Button', () => {
-    it.skip('should render correctly', () => { });    // it = test
+describe('Button component', () => {
+    it.skip('should render correctly', () => {});    // it = test
     it.todo('should display dialog');
     it('should hide popup', () => {
         expect(1).toBe(1);
