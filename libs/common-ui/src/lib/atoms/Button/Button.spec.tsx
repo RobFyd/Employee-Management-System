bc923b64ff325574e7db06c4cc9db82eb743ffe6
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-11 23:54:48 +0100

    chore: Update Button component test to use render method from @testing-library/react

--- libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
@@ -1,14 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { Button } from './Button';
 
 describe('Button component', () => {
-    it.skip('should render correctly', () => {  // it = test
+    it('should render correctly', () => {  // it = test
         const { debug } = render(<Button label="Submit" />);
         debug();
+        expect(screen.getByText('Submit')).toBeInTheDocument();
     });
     // it.todo('should display dialog');
     // it('should hide popup', () => {
     //     expect(1).toBe(1);
     // });
 });              
+
+// npx nx test common-ui --testFile=Button
