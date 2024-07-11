d4d15e2664cada03501557baa00e3bdebda6f53d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-12 00:12:16 +0100

    axe added

--- libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
 
 import { Button } from './Button';
 
 describe('Button component', () => {
     it('should render correctly', () => {  // it = test
-        const { debug } = render(<Button label="Submit!" />);
-        debug();
+        const { debug, container } = render(<Button label="Submit!" />);
+        // debug();
+        console.log(container);
         expect(screen.getByText('Submit', { exact: false })).toBeInTheDocument();
     });
     // it.todo('should display dialog');
