de2ad455fd316f01297e7e41632274608bf2e6dc
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-11 23:36:43 +0100

    chore: Refactor Button component test to use render method from @testing-library/react

--- libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
+++ libs/common-ui/src/lib/atoms/Button/Button.spec.tsx
@@ -1,7 +1,14 @@
+import { render } from '@testing-library/react';
+
+import { Button } from './Button';
+
 describe('Button component', () => {
-    it.skip('should render correctly', () => {});    // it = test
-    it.todo('should display dialog');
-    it('should hide popup', () => {
-        expect(1).toBe(1);
+    it.skip('should render correctly', () => {  // it = test
+        const { debug } = render(<Button label="Submit" />);
+        debug();
     });
+    // it.todo('should display dialog');
+    // it('should hide popup', () => {
+    //     expect(1).toBe(1);
+    // });
 });              
