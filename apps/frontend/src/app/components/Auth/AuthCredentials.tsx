6dc8fd5e9e1dc5b7d4f2912835e52f8c2334ba1f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-14 23:54:55 +0100

    chore: Refactor AuthCredentials component to use useContext hook

--- apps/frontend/src/app/components/Auth/AuthCredentials.tsx
+++ apps/frontend/src/app/components/Auth/AuthCredentials.tsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+
 export const AuthCredentials = () => {
+    const context = useContext();
     return (
         <div>
             <h3>AuthCredentials</h3>
+            <p></p>
         </div>
     );
 }
