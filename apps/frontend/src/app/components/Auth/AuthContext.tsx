aa38cfdc146f53b954dbc3b4b42ab5470ee8e4a4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-14 23:42:38 +0100

    feat: Add AuthContext component for managing authentication state

---
+++ apps/frontend/src/app/components/Auth/AuthContext.tsx
@@ -0,0 +1,3 @@
+// createContext() is a function that creates a Context object. When React renders a component that subscribes to this Context object it will read the current context value from the closest matching Provider above it in the tree.
+// useContext() is a hook that accepts a context object (the value returned from React.createContext) and returns the current context value for that context. The current context value is determined by the value prop of the nearest <MyContext.Provider> above the calling component in the tree.
+
