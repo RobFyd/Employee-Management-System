378614d5fcecd42bd5b087d1d837e4ef6361cca7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-15 23:51:52 +0100

    chore: Update AuthCredentials component

--- apps/frontend/src/app/components/Auth/AuthCredentials.tsx
+++ apps/frontend/src/app/components/Auth/AuthCredentials.tsx
@@ -1,12 +1,19 @@
-import { useContext } from "react";
+import { MouseEventHandler, useContext } from "react";
 import { AuthContext } from "./AuthContext";
+import { Button } from "@ems/common-ui";
 
 export const AuthCredentials = () => {
     const context = useContext(AuthContext);
+
+    const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
+        context.setIsLogged((value) => !value);
+    }
+
     return (
         <div>
             <h3>AuthCredentials</h3>
             <p>Is user logged? {context.isLoggedIn ? 'YES' : 'NO'}</p>
+            <Button onClick={handleClick} label='Toggle' />
         </div>
     );
 };
