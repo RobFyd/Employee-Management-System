4050ce5fa5678e0228c756cc8e20c6d671ccb9d0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-15 23:51:18 +0100

    chore: Update AuthContext to include setIsLogged property

--- apps/frontend/src/app/components/Auth/AuthContext.tsx
+++ apps/frontend/src/app/components/Auth/AuthContext.tsx
@@ -1,12 +1,14 @@
 // createContext() is a function that creates a Context object. When React renders a component that subscribes to this Context object it will read the current context value from the closest matching Provider above it in the tree.
 // useContext() is a hook that accepts a context object (the value returned from React.createContext) and returns the current context value for that context. The current context value is determined by the value prop of the nearest <MyContext.Provider> above the calling component in the tree.
 
-import { createContext } from 'react';
+import { createContext, Dispatch, SetStateAction } from 'react';
 
 interface AuthContextType {
     isLoggedIn: boolean;
+    setIsLogged: Dispatch<SetStateAction<boolean>>
 }
 
 export const AuthContext = createContext<AuthContextType>({
     isLoggedIn: false,
+    setIsLogged: () => null,
 });
