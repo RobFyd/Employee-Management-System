19e77db1aa5c66905db96af7c32d1020614e350b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-16 00:17:47 +0100

    chore: Update AuthContext to include useState hook for isLoggedIn state

--- apps/frontend/src/app/components/Auth/AuthContext.tsx
+++ apps/frontend/src/app/components/Auth/AuthContext.tsx
@@ -1,7 +1,7 @@
 // createContext() is a function that creates a Context object. When React renders a component that subscribes to this Context object it will read the current context value from the closest matching Provider above it in the tree.
 // useContext() is a hook that accepts a context object (the value returned from React.createContext) and returns the current context value for that context. The current context value is determined by the value prop of the nearest <MyContext.Provider> above the calling component in the tree.
 
-import { createContext, Dispatch, SetStateAction } from 'react';
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react';
 
 interface AuthContextType {
     isLoggedIn: boolean;
@@ -12,3 +12,12 @@ export const AuthContext = createContext<AuthContextType>({
     isLoggedIn: false,
     setIsLogged: () => null,
 });
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+    const [isLoggedIn, setIsLogged] = useState(false);
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, setIsLogged }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
