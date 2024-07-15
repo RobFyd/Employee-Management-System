ed9ebb5357b23425d9dc2a5822800f25c1e87c2d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-15 23:55:08 +0100

    feat: Add isLoggedIn state to HomePage component

--- apps/frontend/src/app/pages/HomePage.tsx
+++ apps/frontend/src/app/pages/HomePage.tsx
@@ -1,13 +1,15 @@
 import { Header } from '@ems/common-ui'
 import { AuthInfo } from '../components/Auth/AuthInfo';
 import { AuthContext } from '../components/Auth/AuthContext';
+import { useState } from 'react';
 
 export const HomePage = () => {
+    const [isLoggedIn, setIsLogged] = useState(true);
     return (
         <div>
             <Header>Home</Header>
             <p>Welcome to the home page!</p>
-            <AuthContext.Provider value={{ isLoggedIn: true }}>
+            <AuthContext.Provider value={{ isLoggedIn, setIsLogged }}>
                 <AuthInfo />
             </AuthContext.Provider>
         </div>
