3a4a748d764d4ad581a02980edae219efd07f9c3
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-19 23:51:58 +0100

    chore: Add toggle value test for AuthCredentials component

--- apps/frontend/src/app/components/Auth/AuthCredentials.spec.tsx
+++ apps/frontend/src/app/components/Auth/AuthCredentials.spec.tsx
@@ -1,15 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { AuthCredentials } from './AuthCredentials';
 import { AuthContextProvider } from './AuthContext';
 
 describe('AuthCredentials component', () => {
     it('should toggle value', () => {
-        render(
+        const { debug } = render(
             <AuthContextProvider>
                 <AuthCredentials />
             </AuthContextProvider>
         );
         expect(screen.getByText(/no/i)).toBeInTheDocument();
         // expect(screen.getByText('NO', {exact: false})).toBeInTheDocument();
+        fireEvent.click(screen.get);
+        debug();
     });
 });
+
+// 4 min
