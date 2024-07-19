b25e33b9f9a6601f1cd449d75df158a2e498dd00
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-19 01:01:05 +0100

    chore: Add AuthCredentials component test file and update AuthCredentials.spec.tsx

--- apps/frontend/src/app/components/Auth/AuthCredentials.spec.tsx
+++ apps/frontend/src/app/components/Auth/AuthCredentials.spec.tsx
@@ -1,8 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { AuthCredentials } from './AuthCredentials';
+import { AuthContextProvider } from './AuthContext';
 
 describe('AuthCredentials component', () => {
     it('should toggle value', () => {
-        render(<AuthCredentials />);
+        render(
+            <AuthContextProvider>
+                <AuthCredentials />
+            </AuthContextProvider>
+        );
+        expect(screen.getByText(/no/i)).toBeInTheDocument();
+        // expect(screen.getByText('NO', {exact: false})).toBeInTheDocument();
     });
 });
