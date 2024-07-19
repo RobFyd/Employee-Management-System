704677240d9485a001a33ed6039f1e9ca11c5c61
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-20 00:31:56 +0100

    userEvent added

--- apps/frontend/src/app/components/Auth/AuthCredentials.spec.tsx
+++ apps/frontend/src/app/components/Auth/AuthCredentials.spec.tsx
@@ -1,9 +1,10 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { AuthCredentials } from './AuthCredentials';
 import { AuthContextProvider } from './AuthContext';
 
 describe('AuthCredentials component', () => {
-    it('should toggle value', () => {
+    it('should toggle value', async () => {
         const { debug } = render(
             <AuthContextProvider>
                 <AuthCredentials />
@@ -11,10 +12,9 @@ describe('AuthCredentials component', () => {
         );
         expect(screen.getByText(/no/i)).toBeInTheDocument();
         // expect(screen.getByText('NO', {exact: false})).toBeInTheDocument();
-        fireEvent.click(screen.getByRole('button'));
+        await userEvent.click(screen.getByRole('button'));
+        // fireEvent.click(screen.getByRole('button'));
         expect(screen.getByText(/yes/i)).toBeInTheDocument();
         debug();
     });
 });
-
-// 4 min
