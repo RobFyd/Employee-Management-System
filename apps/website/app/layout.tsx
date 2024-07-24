bd1f5a241bc5038f41cf946a563c74addcac7592
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-25 00:01:46 +0100

    chore: Update body class in layout.tsx to use Inter font

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className}`}>
         <Menu />
         {children}
       </body>
