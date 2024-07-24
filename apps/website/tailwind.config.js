092fddef08aa84ddb2cd04cbb215ef591ff8a1ae
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-24 23:50:40 +0100

    chore: Update tailwind.config.js to extend defaultTheme with Inter var font

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,18 +1,24 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: [
     join(
       __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
