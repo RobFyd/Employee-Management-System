42008d76438d4eb4839258c29ce8140be13a5c26
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-22 00:06:24 +0100

    refactor: Update Header component to use h1 tag instead of header tag

--- libs/common-ui/src/lib/atoms/Header/Header.tsx
+++ libs/common-ui/src/lib/atoms/Header/Header.tsx
@@ -4,7 +4,7 @@ type Props = Readonly<{
   children: string | undefined;
 }>;
 
-export const Header = ({ children }: ComponentProps<'header'> & Props) => {
-  return <header className="text-3xl font-bold my-3">{children}</header>;
+export const Header = ({ children }: ComponentProps<'h1'> & Props) => {
+  return <h1 className="text-3xl font-bold my-2">{children}</h1>;
   // return <header className="text-3xl font-bold my-3 dark:text-white">{children}</header>;
 };
