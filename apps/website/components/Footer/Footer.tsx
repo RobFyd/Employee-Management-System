98bd42f6a7dfc8edfe23927b49ee69584675abe7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-23 23:51:53 +0100

    feat: Add Footer component to website

---
+++ apps/website/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="container border-t-slate-800 py-4">
+            <p className="text-center text-lg text-black">
+                © 2021 All rights reserved
+            </p>
+        </footer>
+    )
+};
