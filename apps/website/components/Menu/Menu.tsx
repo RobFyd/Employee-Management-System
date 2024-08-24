a55d83a3b82ec4233ab15e1d884eddab8cccba34
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-24 23:58:46 +0100

    refactor: Update Menu component to use white text color for active links

--- apps/website/components/Menu/Menu.tsx
+++ apps/website/components/Menu/Menu.tsx
@@ -2,34 +2,38 @@ import Link from 'next/link';
 
 export const Menu = () => {
   return (
-    <nav className="container bg-slate-800">
-      <ul className="flex">
-        <li className="mr-6">
-          <Link href="/about" className="text-lg text-white">
-            About
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link href="/job-offers" className="text-lg text-white">
-            Job offers
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link href="/reviews" className="text-lg text-white">
-            Reviews
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link href="/contact" className="text-lg text-white">
-            Contact
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link href="/dashboard" className="text-lg text-white">
-            Dashboard
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <nav className="bg-slate-900 text-white">
+      <div className="container mx-auto p-4">
+        <div>
+          <ul className="flex">
+            <li className="mr-6">
+              <Link href="/about" className="text-lg text-white">
+                About
+              </Link>
+            </li>
+            <li className="mr-6">
+              <Link href="/job-offers" className="text-lg text-white">
+                Job offers
+              </Link>
+            </li>
+            <li className="mr-6">
+              <Link href="/reviews" className="text-lg text-white">
+                Reviews
+              </Link>
+            </li>
+            <li className="mr-6">
+              <Link href="/contact" className="text-lg text-white">
+                Contact
+              </Link>
+            </li>
+            <li className="mr-6">
+              <Link href="/dashboard" className="text-lg text-white">
+                Dashboard
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav >
   );
 };
