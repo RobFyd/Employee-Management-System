5f3efbdc2be77191db97e2886890bbec5e79b544
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-24 23:55:05 +0100

    refactor: Update Menu component to use white text color for active links

--- apps/website/components/Menu/Menu.tsx
+++ apps/website/components/Menu/Menu.tsx
@@ -2,30 +2,30 @@ import Link from 'next/link';
 
 export const Menu = () => {
   return (
-    <nav className="container border-b-slate-800">
+    <nav className="container bg-slate-800">
       <ul className="flex">
         <li className="mr-6">
-          <Link href="/about" className="text-lg text-black">
+          <Link href="/about" className="text-lg text-white">
             About
           </Link>
         </li>
         <li className="mr-6">
-          <Link href="/job-offers" className="text-lg text-black">
+          <Link href="/job-offers" className="text-lg text-white">
             Job offers
           </Link>
         </li>
         <li className="mr-6">
-          <Link href="/reviews" className="text-lg text-black">
+          <Link href="/reviews" className="text-lg text-white">
             Reviews
           </Link>
         </li>
         <li className="mr-6">
-          <Link href="/contact" className="text-lg text-black">
+          <Link href="/contact" className="text-lg text-white">
             Contact
           </Link>
         </li>
         <li className="mr-6">
-          <Link href="/dashboard" className="text-lg text-black">
+          <Link href="/dashboard" className="text-lg text-white">
             Dashboard
           </Link>
         </li>
