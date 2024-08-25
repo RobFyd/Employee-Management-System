c4a4ad00364f2a842e46cb8fe5c9403b0681e109
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-26 00:10:39 +0100

    file fonts.ts added

---
+++ apps/website/app/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto, Galindo } from 'next/font/google';
+
+//export const inter = Inter({ subsets: ['latin'] });
+//export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
+export const galindo = Galindo({ weight: ['400'], subsets: ['latin'] });
