c6ec05d6c13913edd7714614570eb8eb4a56fa2f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-26 00:16:10 +0100

    feat: Add Montserrat font to website
    This commit adds the Montserrat font to the website by importing it from the 'next/font/google' package. The font is now available for use in the website's styling.

--- apps/website/app/fonts.ts
+++ apps/website/app/fonts.ts
@@ -1,5 +1,6 @@
-import { Inter, Roboto, Galindo } from 'next/font/google';
+import { Inter, Roboto, Galindo, Montserrat } from 'next/font/google';
 
-//export const inter = Inter({ subsets: ['latin'] });
-//export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
 export const galindo = Galindo({ weight: ['400'], subsets: ['latin'] });
+export const montserrat = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
