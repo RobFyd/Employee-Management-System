16c49210546edbe80b6310f0ff21a30b656840d4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-26 23:40:27 +0100

    Image from next added

--- apps/website/app/(public)/contact/page.tsx
+++ apps/website/app/(public)/contact/page.tsx
@@ -1,11 +1,12 @@
 import { Header } from '@ems/common-ui';
+import Image from 'next/image';
 
 export default function ContactPage() {
     return (
         <div>
             <Header>Contact</Header>
             <p>This is the contact page.</p>
-            <img src="/nachos.png" />
+            <Image src="/nachos.png" alt="Nachos" width={600} height={400} />
         </div>
     );
 }
