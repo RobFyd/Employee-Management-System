5888576748c7d8222331fa650cf5d7c5e7ad16ea
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-30 00:52:54 +0100

    refactor: Update ReviewsList component to fetch reviews from Airtable API
    This commit updates the ReviewsList component to fetch reviews from the Airtable API instead of the local server. It also adds the necessary headers for authorization and sorts the reviews by the created_at field in descending order.

--- apps/website/app/(public)/reviews/ReviewsList.tsx
+++ apps/website/app/(public)/reviews/ReviewsList.tsx
@@ -9,12 +9,24 @@ type ReviewDto = {
     created_at: string;
 }
 
+// curl "https://api.airtable.com/v0/appWX9InzcayFH6G4/Reviews?maxRecords=3&view=Grid%20view" \
+// -H "Authorization: Bearer YOUR_SECRET_API_TOKEN"
+
+// sort%5B0%5D%5Bfield%5D=content
+// sort%5B0%5D%5Bdirection%5D=desc
+
 export const ReviewsList = () => {
     const [reviews, setReviews] = useState<ReviewDto[] | undefined>(undefined);
 
+    console.log('token: ', process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN);
+
     useEffect(() => {
-        fetch('/api/reviews')
-            .then(response => response.json())
-            .then(data => setReviews(data));
+        fetch('https://api.airtable.com/v0/appWX9InzcayFH6G4/Reviews?view=Grid%20view&sort%5B0%5D%5Bfield%5D=created_at&sort%5B0%5D%5Bdirection%5D=desc', {
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN}`,
+            }
+        })
     }, []);
+
+    return <div></div>;
 }
--- apps/website/app/(public)/reviews/page.tsx
+++ apps/website/app/(public)/reviews/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@ems/common-ui';
+import { ReviewsList } from './ReviewsList';
 
 export default function ReviewsPage() {
     return (
         <div>
             <Header>Reviews</Header>
             <p>This is the reviews page.</p>
+            <ReviewsList />
         </div>
     );
 }
