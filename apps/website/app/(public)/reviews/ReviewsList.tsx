8d0b3cd0c294461a8d0b7aed6dffe96feaec2011
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-08-31 00:30:48 +0100

    refactor: Update ReviewsList component to fetch reviews from Airtable API

--- apps/website/app/(public)/reviews/ReviewsList.tsx
+++ apps/website/app/(public)/reviews/ReviewsList.tsx
@@ -2,12 +2,23 @@
 
 import { useState, useEffect } from "react";
 
-type ReviewDto = {
+type Review = {
     id: string;
     content: string;
     author: string;
     created_at: string;
 }
+
+type AirtableReviewResponseDto = {
+    records: {
+        id: string;
+        fields: {
+            content: string;
+            author: string;
+            created_at: string;
+        };
+    }[];
+};
 
 // curl "https://api.airtable.com/v0/appWX9InzcayFH6G4/Reviews?maxRecords=3&view=Grid%20view" \
 // -H "Authorization: Bearer YOUR_SECRET_API_TOKEN"
@@ -16,7 +27,7 @@ type ReviewDto = {
 // sort%5B0%5D%5Bdirection%5D=desc
 
 export const ReviewsList = () => {
-    const [reviews, setReviews] = useState<ReviewDto[] | undefined>(undefined);
+    const [reviews, setReviews] = useState<Review[] | undefined>(undefined);
 
     console.log('token: ', process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN);
 
@@ -37,11 +48,25 @@ export const ReviewsList = () => {
     // }, []);
 
     useEffect(() => {
-        fetch('https://api.airtable.com/v0/appWX9InzcayFH6G4/Reviews&view=default?sort%5B0%5D%5Bfield%5D=created_at', {
+        fetch('https://api.airtable.com/v0/appWX9InzcayFH6G4/Reviews?view=default&sort%5B0%5D%5Bfield%5D=created_at&sort%5B0%5D%5Bdirection%5D=desc', {
             headers: {
                 Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN}`,
             }
-        })
+        }
+        )
+            .then((response) => response.json())
+            .then((data: AirtableReviewResponseDto) => {
+                const _reviews: Review[] = [];
+                data.records.forEach((elem) => {
+                    _reviews.push({
+                        id: elem.id,
+                        content: elem.fields.content,
+                        author: elem.fields.author,
+                        created_at: elem.fields.created_at,
+                    });
+                });
+                setReviews(_reviews);
+            });
     }, []);
 
     return <div></div>;
