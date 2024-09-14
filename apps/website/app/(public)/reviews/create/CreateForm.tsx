1915f501a19b1a25e5167153e05fd150ba7aac48
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-14 23:33:25 +0100

    CreateForm added

--- apps/website/app/(public)/reviews/create/CreateForm.tsx
+++ apps/website/app/(public)/reviews/create/CreateForm.tsx
@@ -8,3 +8,15 @@ import { useRouter } from "next/router";
 import { Button, Input } from "@ems/common-ui";
 
 import { createReview } from "./actions";
+
+export const CreateForm = () => {
+    const [isError, setIsError] = useState(false);
+
+    const ClientAction = async (formData: FormData) => {
+        const serverResult = await createReview(formData);
+        console.loglog({ serverResult });
+        if (serverResult.status === 'error') {
+            setIsError(true);
+        }
+    };
+};
