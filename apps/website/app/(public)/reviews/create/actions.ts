548efaf6cc7b926b685ca140111c67fa6e972c83
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-20 00:44:08 +0100

    Refactor CreateForm to use next/navigation instead of next/router

--- apps/website/app/(public)/reviews/create/actions.ts
+++ apps/website/app/(public)/reviews/create/actions.ts
@@ -1,28 +1,26 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+import { createReviewInAirtable } from '../services';
 import { CreateReviewDto, createReviewSchema } from '../types';
 
-// export const createReview = async (formData: FormData) => {
-  export const createReview = async (review: CreateReviewDto) => {
-  'use server';
+export const createReview = async (formData: FormData) => {
+  const rawReview: CreateReviewDto = {
+    content: formData.get('content') as string,
+    author: formData.get('author') as string,
+  };
 
-  // const review: CreateReviewDto = {
-  //   content: formData.get('content') as string,
-  //   author: formData.get('author') as string,
-  //   // points: parseInt(formData.get('points') as string),
-  // };
-
-  const result = createReviewSchema.safeParse(review);
+  const result = createReviewSchema.safeParse(rawReview);
   if (!result.success) {
-    console.log(result.error.issues);
+    // console.log(result.error.issues);
 
     return {
-      status: 'error',
+      error: true,
     };
   } else {
-    return {
-      status: 'success',
-      payload: review,
-    };
+    await createReviewInAirtable(rawReview);
+
+    // revalidatePath('/reviews');
+    redirect('/reviews');
   }
 };
