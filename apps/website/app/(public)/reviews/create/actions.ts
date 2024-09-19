817b1d31e2eb35c3b5642962e48a19622ec8d72e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-19 23:44:33 +0100

    actions.ts refactored

--- apps/website/app/(public)/reviews/create/actions.ts
+++ apps/website/app/(public)/reviews/create/actions.ts
@@ -2,14 +2,15 @@
 
 import { CreateReviewDto, createReviewSchema } from '../types';
 
-export const createReview = async (formData: FormData) => {
+// export const createReview = async (formData: FormData) => {
+  export const createReview = async (review: CreateReviewDto) => {
   'use server';
 
-  const review: CreateReviewDto = {
-    content: formData.get('content') as string,
-    author: formData.get('author') as string,
-    points: parseInt(formData.get('points') as string),
-  };
+  // const review: CreateReviewDto = {
+  //   content: formData.get('content') as string,
+  //   author: formData.get('author') as string,
+  //   // points: parseInt(formData.get('points') as string),
+  // };
 
   const result = createReviewSchema.safeParse(review);
   if (!result.success) {
