e2b35f39e97d86865ef9c847d30a2c36c09b1c91
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-20 00:44:14 +0100

    Refactor CreateForm to use next/navigation instead of next/router

--- apps/website/app/(public)/reviews/create/CreateForm.tsx
+++ apps/website/app/(public)/reviews/create/CreateForm.tsx
@@ -1,40 +1,25 @@
 'use client';
 
-import { useState, useTransition } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
+import { useState } from 'react';
 
-import { Button, Input } from "@ems/common-ui";
-
-import { createReview } from "./actions";
-import { CreateReviewDto, createReviewSchema } from "../types";
+import { Button, Input } from '@ems/common-ui';
+import { createReview } from './actions';
 
 export const CreateForm = () => {
     const [isError, setIsError] = useState(false);
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<CreateReviewDto>({
-        resolver: zodResolver(createReviewSchema),
-    });
 
-    const clientAction: SubmitHandler<CreateReviewDto> = async (data) => {
-        const serverResult = await createReview(data);
-        console.log({ serverResult });
-        if (serverResult.status === 'error') {
-            setIsError(true);
-        }
+    const clientAction = async (formData: FormData) => {
+        const serverResult = await createReview(formData);
+        // console.log({ serverResult });
+        setIsError(serverResult.error);
     };
 
     return (
         <>
             {isError && <p>Oh no server error!</p>}
-            <form onSubmit={handleSubmit(clientAction)}>
-                <Input {...register('content')} label="Content" error={errors.content} />
-                <Input {...register('author')} label="Author" error={errors.author} />
-                <Input {...register('points')} label="Points" error={errors.points} />
+            <form action={clientAction}>
+                <Input label="Content" name="content" />
+                <Input label="Author" name="author" />
                 <Button label="Submit" type="submit" />
             </form>
         </>
