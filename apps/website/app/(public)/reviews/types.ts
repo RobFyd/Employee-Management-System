c8ed1143fb50416000c2effe4d578ac9bf1bc9f7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-20 00:43:56 +0100

    Refactor types.ts to update CreateReviewDto schema

--- apps/website/app/(public)/reviews/types.ts
+++ apps/website/app/(public)/reviews/types.ts
@@ -1,20 +1,17 @@
 import { z } from 'zod';
 
 export type Review = {
-    id: string;
-    content: string;
-    author: string;
-    points: number;
-    created_at: string;
-}
+  id: string;
+  content: string;
+  author: string;
+  created_at: string;
+};
 
-// export type CreateReviewDto = Pick<Review, 'content' | 'author' | 'points'>;
+// export type CreateReviewDto = Pick<Review, 'author' | 'content'>;
 
 export const createReviewSchema = z.object({
-    content: z.string().min(1, 'Content is required'),
-    author: z.string().min(1, 'Author is required'),
-    points: z.number().int().min(1, 'Points is required'),
+  content: z.string().min(1, 'Content is required'),
+  author: z.string().min(1, 'Author is required'),
 });
 
 export type CreateReviewDto = z.infer<typeof createReviewSchema>;
-    
