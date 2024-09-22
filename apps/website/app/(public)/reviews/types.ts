619a9486e0802e8cb558917a55dcc8a2543e64d2
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-22 19:51:49 +0100

    website types.ts updated

--- apps/website/app/(public)/reviews/types.ts
+++ apps/website/app/(public)/reviews/types.ts
@@ -4,14 +4,16 @@ export type Review = {
   id: string;
   content: string;
   author: string;
+  points: number;
   created_at: string;
 };
 
-// export type CreateReviewDto = Pick<Review, 'author' | 'content'>;
+// export type CreateReviewDto = Pick<Review, 'content' | 'author' | 'points'>;
 
 export const createReviewSchema = z.object({
   content: z.string().min(1, 'Content is required'),
   author: z.string().min(1, 'Author is required'),
+  points: z.number().int().min(1, 'Points is required'),
 });
 
 export type CreateReviewDto = z.infer<typeof createReviewSchema>;
