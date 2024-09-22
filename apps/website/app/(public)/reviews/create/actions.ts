5906d0f47e90d79a09b927941877cf1f6155f64d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-22 19:52:10 +0100

    website actions.ts updated

--- apps/website/app/(public)/reviews/create/actions.ts
+++ apps/website/app/(public)/reviews/create/actions.ts
@@ -1,28 +1,27 @@
 'use server';
 
-import { redirect } from 'next/navigation';
-import { createReviewInAirtable } from '../services';
 import { CreateReviewDto, createReviewSchema } from '../types';
 
-// export const createReview = async (formData: FormData) => {
-export const createReview = async (data: CreateReviewDto) => {
-  // const rawReview: CreateReviewDto = {
-  //   content: formData.get('content') as string,
-  //   author: formData.get('author') as string,
-  // };
-  const rawReview: CreateReviewDto = data;
+export const createReview = async (formData: FormData) => {
+  'use server';
 
-  const result = createReviewSchema.safeParse(rawReview);
+  const review: CreateReviewDto = {
+    content: formData.get('content') as string,
+    author: formData.get('author') as string,
+    points: parseInt(formData.get('points') as string),
+  };
+
+  const result = createReviewSchema.safeParse(review);
   if (!result.success) {
-    // console.log(result.error.issues);
+    console.log(result.error.issues);
 
     return {
-      error: true,
+      status: 'error',
     };
   } else {
-    await createReviewInAirtable(rawReview);
-
-    // revalidatePath('/reviews');
-    // redirect('/reviews');
+    return {
+      status: 'success',
+      payload: review,
+    };
   }
 };
