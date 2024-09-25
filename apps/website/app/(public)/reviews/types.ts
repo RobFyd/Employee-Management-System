1985213df4b0b7865dac70d5b07bf487a59bcc4b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-25 23:56:28 +0100

    Refactor Review type and schema to use string for points instead of number

--- apps/website/app/(public)/reviews/types.ts
+++ apps/website/app/(public)/reviews/types.ts
@@ -4,18 +4,16 @@ export type Review = {
   id: string;
   content: string;
   author: string;
-  //points: number;
-  points: string;
+  points: number;
+  // points: string;
   created_at: string;
 };
-
-// export type CreateReviewDto = Pick<Review, 'content' | 'author' | 'points'>;
 
 export const createReviewSchema = z.object({
   content: z.string().min(1, 'Content is required'),
   author: z.string().min(1, 'Author is required'),
-  // points: z.number().int().min(1, 'Points is required'),
-  points: z.string().min(1, 'Points is required'),
+  points: z.number().int().min(1, 'Points is required'),
+  // points: z.string().min(1, 'Points is required'),
 });
 
 export type CreateReviewDto = z.infer<typeof createReviewSchema>;
