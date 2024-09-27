f8c7b3558638e618679807f9277f0da4c172f191
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-28 00:44:20 +0100

    Refactor CreateReview component formatting

--- apps/website/app/(public)/reviews/create/page.tsx
+++ apps/website/app/(public)/reviews/create/page.tsx
@@ -53,10 +53,10 @@ import { Header } from '@ems/common-ui';
 import { CreateForm } from './CreateForm';
 
 export default function CreateReview() {
-    return (
-        <div>
-            <Header>Create review</Header>
-            <CreateForm />
-        </div>
-    );
+  return (
+    <div>
+      <Header>Create review</Header>
+      <CreateForm />
+    </div>
+  );
 }
