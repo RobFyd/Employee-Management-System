3c34de827e239ad4474c68554cd61c73d8c268f4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-27 23:43:38 +0100

    file downgraded

--- apps/website/app/(public)/reviews/create/actions.ts
+++ apps/website/app/(public)/reviews/create/actions.ts
@@ -1,28 +1,52 @@
+// 'use server';
+
+// import { CreateReviewDto, createReviewSchema } from '../types';
+
+// export const createReview = async (formData: FormData) => {
+//   'use server';
+
+//   const review: CreateReviewDto = {
+//     content: formData.get('content') as string,
+//     author: formData.get('author') as string,
+//     points: parseInt(formData.get('points') as string),
+//     // points: formData.get('points') as string,
+//   };
+
+//   const result = createReviewSchema.safeParse(review);
+//   if (!result.success) {
+//     console.log(result.error.issues);
+
+//     return {
+//       status: 'error',
+//     };
+//   } else {
+//     return {
+//       status: 'success',
+//       payload: review,
+//     };
+//   }
+// };
+
 'use server';
 
+import { redirect } from 'next/navigation';
+import { createReviewInAirtable } from '../services';
 import { CreateReviewDto, createReviewSchema } from '../types';
 
 export const createReview = async (formData: FormData) => {
-  'use server';
-
-  const review: CreateReviewDto = {
+  const rawReview: CreateReviewDto = {
     content: formData.get('content') as string,
     author: formData.get('author') as string,
-    points: parseInt(formData.get('points') as string),
-    // points: formData.get('points') as string,
   };
 
-  const result = createReviewSchema.safeParse(review);
+  const result = createReviewSchema.safeParse(rawReview);
   if (!result.success) {
-    console.log(result.error.issues);
-
     return {
-      status: 'error',
+      error: true,
     };
   } else {
-    return {
-      status: 'success',
-      payload: review,
-    };
+    await createReviewInAirtable(rawReview);
+
+    redirect('/reviews');
   }
 };
