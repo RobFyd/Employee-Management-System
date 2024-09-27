07fdacbbf81fbbd8c07ce9628353065db479c706
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-27 23:43:25 +0100

    file downgraded

--- apps/website/app/(public)/reviews/types.ts
+++ apps/website/app/(public)/reviews/types.ts
@@ -1,19 +1,36 @@
+// import { z } from 'zod';
+
+// export type Review = {
+//   id: string;
+//   content: string;
+//   author: string;
+//   points: number;
+//   // points: string;
+//   created_at: string;
+// };
+
+// export const createReviewSchema = z.object({
+//   content: z.string().min(1, 'Content is required'),
+//   author: z.string().min(1, 'Author is required'),
+//   points: z.number().int().min(1, 'Points is required'),
+//   // points: z.string().min(1, 'Points is required'),
+// });
+
+// export type CreateReviewDto = z.infer<typeof createReviewSchema>;
+
+
 import { z } from 'zod';
 
 export type Review = {
   id: string;
   content: string;
   author: string;
-  points: number;
-  // points: string;
   created_at: string;
 };
 
 export const createReviewSchema = z.object({
   content: z.string().min(1, 'Content is required'),
   author: z.string().min(1, 'Author is required'),
-  points: z.number().int().min(1, 'Points is required'),
-  // points: z.string().min(1, 'Points is required'),
 });
 
 export type CreateReviewDto = z.infer<typeof createReviewSchema>;
