1630ed8cd2e23b3b3a7f621a2c585d852f4a39c0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-27 23:43:14 +0100

    file downgraded

--- apps/website/app/(public)/reviews/services.ts
+++ apps/website/app/(public)/reviews/services.ts
@@ -1,12 +1,63 @@
-// import { unstable_noStore as noStore } from 'next/cache';
+// // import { unstable_noStore as noStore } from 'next/cache';
+// import { format } from 'date-fns';
+// import { CreateReviewDto } from './types';
+// type Review = {
+//   id: string;
+//   content: string;
+//   author: string;
+//   created_at: string;
+// };
+
+// type AirtableReviewResponseDto = {
+//   records: {
+//     id: string;
+//     fields: {
+//       content: string;
+//       author: string;
+//       created_at: string;
+//     };
+//   }[];
+// };
+
+// export const fetchReviews = async () => {
+//   // noStore();
+//   const response = await fetch(
+//     `${process.env.AIRTABLE_BASE_URL}/Reviews?view=default&sort%5B0%5D%5Bfield%5D=created_at&sort%5B0%5D%5Bdirection%5D=asc`,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${process.env.AIRTABLE_API_TOKEN}`,
+//       },
+//     }
+//   );
+//   const data: AirtableReviewResponseDto = await response.json();
+//   const reviews: Review[] = [];
+//   data.records.forEach((elem) => {
+//     reviews.push({
+//       id: elem.id,
+//       content: elem.fields.content,
+//       author: elem.fields.author,
+//       created_at: format(elem.fields.created_at, 'hh:mm:ss dd/MM/yyyy'),
+//     });
+//   });
+//   return reviews;
+// };
+
+// export const createReviewInAirtable = async (review: CreateReviewDto) => {
+//   const response = await fetch(`${process.env.AIRTABLE_BASE_URL}/reviews`, {
+//     headers: {
+//       Authorization: `Bearer ${process.env.AIRTABLE_API_TOKEN}`,
+//       'Content-type': 'application/json',
+//     },
+//     method: 'POST',
+//     body: JSON.stringify({ records: [{ fields: review }] }),
+//   });
+//   const data = await response.json();
+
+//   console.log('createReviewInAirtable', { data: data.records[0] });
+// };
+
 import { format } from 'date-fns';
-import { CreateReviewDto } from './types';
-type Review = {
-  id: string;
-  content: string;
-  author: string;
-  created_at: string;
-};
+import { CreateReviewDto, Review } from './types';
 
 type AirtableReviewResponseDto = {
   records: {
@@ -20,9 +71,8 @@ type AirtableReviewResponseDto = {
 };
 
 export const fetchReviews = async () => {
-  // noStore();
   const response = await fetch(
-    `${process.env.AIRTABLE_BASE_URL}/Reviews?view=default&sort%5B0%5D%5Bfield%5D=created_at&sort%5B0%5D%5Bdirection%5D=asc`,
+    `${process.env.AIRTABLE_BASE_URL}/reviews?view=default&sort%5B0%5D%5Bfield%5D=created_at&sort%5B0%5D%5Bdirection%5D=desc`,
     {
       headers: {
         Authorization: `Bearer ${process.env.AIRTABLE_API_TOKEN}`,
@@ -36,9 +86,10 @@ export const fetchReviews = async () => {
       id: elem.id,
       content: elem.fields.content,
       author: elem.fields.author,
-      created_at: format(elem.fields.created_at, 'hh:mm:ss dd/MM/yyyy'),
+      created_at: format(elem.fields.created_at, 'dd.MM.yyyy HH:mm:ss'),
     });
   });
+
   return reviews;
 };
 
@@ -53,5 +104,5 @@ export const createReviewInAirtable = async (review: CreateReviewDto) => {
   });
   const data = await response.json();
 
-  console.log('createReviewInAirtable', { data: data.records[0] });
+  console.log({ data });
 };
