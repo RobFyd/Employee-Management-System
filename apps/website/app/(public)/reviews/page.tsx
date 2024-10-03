a913e7e4efe36988f690aa99f700158a936861d6
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-04 00:19:14 +0100

    Refactor fetchReviewsCount function to include count of reviews in ReviewsPage

--- apps/website/app/(public)/reviews/page.tsx
+++ apps/website/app/(public)/reviews/page.tsx
@@ -1,16 +1,19 @@
 import { Header } from '@ems/common-ui';
-import { fetchReviews } from './services';
+import { fetchReviews, fetchReviewsCount } from './services';
 import Link from 'next/link';
 
 export default async function ReviewsPage() {
     // await new Promise((resolve) => setTimeout(resolve, 2000));
     const reviews = await fetchReviews();
+    const reviewsCount = await fetchReviewsCount();
 
     return (
         <div>
             <Header>Reviews</Header>
             <p>This is the reviews page.</p>
-            <Link href="/reviews/create" className="my-3 block text-blue-600">Create a review</Link>
+            <Link href="/reviews/create" className="my-3 block text-blue-600 flex justify-center">Create a review</Link>
+
+            <h3 className="mb-4 flex justify-center">Count: {reviewsCount}</h3>
 
             <ul>{reviews?.map((elem) => (
                 <li key={elem.id}>
