29a8166be950c1868edc5adb059429dd79912df9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-12 21:43:00 +0100

    Refactor ReviewsList component and integrate with database

--- apps/website/app/(public)/reviews/components/ReviewsList.tsx
+++ apps/website/app/(public)/reviews/components/ReviewsList.tsx
@@ -1,20 +1,38 @@
 import Link from 'next/link';
-import { fetchReviews } from '../services';
+import { fetchReviews, fetchReviews2 } from '../services';
+
+import { format } from 'date-fns';
 
 export default async function ReviewsList() {
     const reviews = await fetchReviews();
+    const reviews2 = await fetchReviews2();
 
     return (
-        <div>
-            <ul>{reviews?.map((elem) => (
-                <li key={elem.id}>
-                    <div className="text-blue-700"><Link href={`
+        <div className="flex flex-row">
+            <div className="mr-20">
+                <h1 className="mb-6">AirTable</h1>
+                <ul>{reviews?.map((elem) => (
+                    <li key={elem.id}>
+                        <div className="text-blue-700"><Link href={`
                         /reviews/${elem.id}
                     `}>{elem.content}</Link></div>
-                    <div className="text-red-900">{elem.author}</div>
-                    <div className="mb-4">{elem.created_at}</div>
-                </li>
-            ))}</ul>
+                        <div className="text-red-900">{elem.author}</div>
+                        <div className="mb-4">{elem.created_at}</div>
+                    </li>
+                ))}</ul>
+            </div>
+            <div>
+                <h1 className="mb-6">Server</h1>
+                <ul>{reviews2?.map((elem) => (
+                    <li key={elem.public_id}>
+                        <div className="text-blue-700"><Link href={`
+                        /reviews/${elem.public_id}
+                    `}>{elem.content}</Link></div>
+                        <div className="text-red-900">{elem.author}</div>
+                        <div className="mb-4">{format(elem.created_at, 'dd.MM.yyyy HH.mm.ss')}</div>
+                    </li>
+                ))}</ul>
+            </div>
         </div>
     );
 }
