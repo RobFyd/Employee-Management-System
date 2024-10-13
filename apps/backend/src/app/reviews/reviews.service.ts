4ae690968371347bdd618ea5a836d74c77f0b96b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-13 22:39:02 +0100

    Refactor ReviewsService to include CRUD operations for reviews

---
+++ apps/backend/src/app/reviews/reviews.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+
+type Review = {
+  id: number;
+  content: string;
+  rating: number;
+};
+
+const reviews: Review[] = [
+  { id: 1, content: 'Review 1', rating: 5 },
+  { id: 2, content: 'Review 2', rating: 4 },
+];
+
+@Injectable()
+export class ReviewsService {
+  getReviews(): Review[] {
+    return reviews;
+  }
+
+  getReview(id: Review['id']): Review {
+    return reviews[0];
+  }
+
+  createReview(data: Review) {
+    reviews.push(data);
+    return data;
+  }
+
+  deleteReview(id: Review['id']) {
+    return null;
+  }
+}
