ef7d99ff0b80a32ab4fa40b97354cdcb018b8212
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-20 20:00:28 +0100

    Refactor HttpExceptionFilter to handle HTTP exceptions and provide a standardized error response

--- apps/backend/src/app/filters/http-exception-filter.ts
+++ apps/backend/src/app/filters/http-exception-filter.ts
@@ -4,12 +4,27 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter<T extends HttpException>
   implements ExceptionFilter
 {
   catch(exception: T, host: ArgumentsHost) {
-    throw new Error('Method not implemented.');
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+
+    response.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+    });
   }
 }
