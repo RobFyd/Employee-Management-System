8f2edcac598a8893ffd2c6ea99759dca35aa064b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-20 20:58:04 +0100

    Add ApiGuard to global guards

---
+++ apps/backend/src/app/guards/api-guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+export class ApiGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+    return authHeader === process.env.API_KEY;
+  }
+}
