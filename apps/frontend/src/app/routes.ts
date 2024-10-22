e080129488d3ab65f2fa8e01a4b3f754b706cf6d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-22 01:13:06 +0100

    Refactor app routes to include new page for creating offers

--- apps/frontend/src/app/routes.ts
+++ apps/frontend/src/app/routes.ts
@@ -1,7 +1,9 @@
 export enum ROUTE {
-    REGISTRATION = '/registration',
-    HOME = '/home',
-    ABOUT = '/about',
-    CONTACT = '/contact',
-    EMPLOYEES = '/employees',
+  REGISTRATION = '/registration',
+  HOME = '/home',
+  ABOUT = '/about',
+  CONTACT = '/contact',
+  EMPLOYEES = '/employees',
+  OFFERS = '/offers',
+  OFFERS_CREATE = '/offers/create',
 }
