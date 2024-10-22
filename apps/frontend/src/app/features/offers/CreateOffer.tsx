1927eb72da463df2bafe385789502b2644572b70
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-23 00:00:07 +0100

    Add form for creating offers in CreateOffer component

--- apps/frontend/src/app/features/offers/CreateOffer.tsx
+++ apps/frontend/src/app/features/offers/CreateOffer.tsx
@@ -3,6 +3,16 @@ export const CreateOffer = () => {
     return (
         <div>
             <h3>Create offer</h3>
+
+            <form onSubmit={handleSubmit(clientAction)}>
+                <Input
+                    {...register('content')}
+                    label="Content"
+                    error={errors.content}
+                />
+                <Input {...register('author')} label="Author" error={errors.author} />
+                <Button label="Submit" type="submit" />
+            </form>
         </div>
     )
 };
