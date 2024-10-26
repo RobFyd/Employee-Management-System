976921ec4c4febb3fb59cf1b35a775e5397b5cf7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-26 22:07:51 +0100

    Refactor OffersController to use string instead of number for offer IDs

--- apps/backend/src/app/offers/offers.controller.ts
+++ apps/backend/src/app/offers/offers.controller.ts
@@ -23,7 +23,7 @@ export class OffersController {
   }
 
   @Get(':id')
-  async getOffer(@Param('id') id: number) {
+  async getOffer(@Param('id') id: string) {
     return await this.offersService.getOffer(id);
   }
 
@@ -33,7 +33,7 @@ export class OffersController {
   }
 
   @Patch(':id')
-  updateOffer(@Param('id') id: number, @Body() updateOfferDto: UpdateOfferDto) {
+  updateOffer(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
     return this.offersService.updateOffer(id, updateOfferDto);
   }
 }
