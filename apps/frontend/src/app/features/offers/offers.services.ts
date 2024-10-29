bcd18fccee804d2482f7a18c6ae045a04897a969
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-29 23:23:42 +0000

    Refactor API endpoint URL in createOffer function

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 import { CreateOfferDto } from './types';
 
-const API_KEY = '1234'; // this should be stored in a .env file
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const createOffer = (data: CreateOfferDto) => {
-  return axios.post('http://localhost:3000/api/offers', data, {
+  return axios.post(`${import.meta.env.VITE_API_BASE_URL}/offers`, data, {
     headers: {
       Authorization: API_KEY,
     },
