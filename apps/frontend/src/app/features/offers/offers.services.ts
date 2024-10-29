cf94d438fa569bac56dd82f15a6ea428a29358c8
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-29 23:34:56 +0000

    Refactor API endpoint URL and authorization header in createOffer function

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -1,13 +1,7 @@
-import axios from 'axios';
+import { api } from '../../config/api';
 
 import { CreateOfferDto } from './types';
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-
 export const createOffer = (data: CreateOfferDto) => {
-  return axios.post(`${import.meta.env.VITE_API_BASE_URL}/offers`, data, {
-    headers: {
-      Authorization: API_KEY,
-    },
-  });
+  return api.post('/offers', data);
 };
