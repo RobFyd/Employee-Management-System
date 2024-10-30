e97db6b9d64ce5eedcb87dc94d350d96f5f763dd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-30 23:45:17 +0000

    Add OffersList component to display OfferListDto data

---
+++ apps/frontend/src/app/features/offers/OffersList.tsx
@@ -0,0 +1,18 @@
+import { OfferListDto } from "@ems/contracts";
+
+type Props = {
+    data: OfferListDto[];
+};
+
+export const OffersList = ({ data }: Props) => {
+    return (
+        <ul>
+            {data.map(elem => (
+                <li key={elem.public_id}>
+                    {elem.role} | {elem.description} | {elem.salary} £
+                </li>
+
+            ))}
+        </ul>
+    );
+};
