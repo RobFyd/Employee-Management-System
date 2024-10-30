2324308967a8ae3f3a9f51a807788e522709c75c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-30 23:12:08 +0000

    Update fetchOffers to return a Promise of OfferListDto array

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -1,11 +1,12 @@
-import { api } from '../../config/api';
+import { OfferListDto } from '@ems/contracts';
 
+import { api } from '../../config/api';
 import { CreateOfferDto } from './types';
 
 export const createOffer = (data: CreateOfferDto) => {
   return api.post('/offers', data);
 };
 
-export const fetchOffers = () => {
+export const fetchOffers = (): Promise<OfferListDto[]> => {
   return api.get('/offers');
 };
