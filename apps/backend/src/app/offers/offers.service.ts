215e0e4eb5ee111e31027998f3d40fe8763e21ce
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-10-30 23:08:42 +0000

    Update getOffers method to return OfferListDto and select specific fields

--- apps/backend/src/app/offers/offers.service.ts
+++ apps/backend/src/app/offers/offers.service.ts
@@ -7,6 +7,8 @@ import {
 
 import { type Offer } from '@prisma/client';
 
+import { OfferListDto } from '@ems/contracts';
+
 import { CreateOfferDto } from './dtos/create-offer.dto';
 import { UpdateOfferDto } from './dtos/update-offer.dto';
 import { PrismaService } from '../prisma.service';
@@ -15,8 +17,16 @@ import { PrismaService } from '../prisma.service';
 export class OffersService {
   constructor(private prisma: PrismaService) {}
 
-  async getOffers(page?: number, offset?: number): Promise<Offer[]> {
-    return await this.prisma.offer.findMany();
+  async getOffers(page?: number, offset?: number): Promise<OfferListDto[]> {
+    return await this.prisma.offer.findMany({
+      select: {
+        public_id: true,
+        role: true,
+        description: true,
+        salary: true,
+        created_at: true,
+      },
+    });
   }
 
   async getOffer(id: Offer['public_id']): Promise<Offer> {
