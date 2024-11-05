7cf6798d4f8a7ec407137e35c77f6ea550923a88
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-11-05 00:07:49 +0000

    Add ApiProperty decorators to DTOs for Swagger documentation

--- apps/backend/src/app/reviews/dtos/create-review.dto.ts
+++ apps/backend/src/app/reviews/dtos/create-review.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, IsPositive } from 'class-validator';
 
 export class CreateReviewDto {
   //   id: number;
 
+  @ApiProperty()
   @IsString()
   content: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   rate: number;
